d9cac9f86bc1e3a55eb150a25d8e3478294f3696
Juda <judaponce14@unitec.edu>
2020-07-03 17:08:22 -0600

    no entendi nada solo algunas clases

--- main.cpp
+++ main.cpp
@@ -4,19 +4,6 @@
 using namespace std;
  
 int main() {
-  int dato,raiz, aux;
- 
-  cout << "Pon un numero para obtener la raiz cuadrada:";
-  cin >> dato;
-  raiz = sqrt(dato);
-  aux=raiz*raiz;
-  if(aux==dato){
-  	cout<<"Es matriz cuadrada";
-  }else{
-  	cout<<"No lo es";
-  }
- 
-  cout << "La raiz cuadrada de " << dato << " es " << raiz << endl;
  
   return 0;
 }
